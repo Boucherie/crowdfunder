6d2b360e32325bd5eb30c04bb9b2ff7fa25fe8de
DocPrior <Sara Prior>
2017-06-26 17:14:49 -0400

    clean out old code from search controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,5 @@
 class SearchesController < ApplicationController
   def search_title
-    # @project = Project.find_by(:title => params[:title])
     @projects = Project.where("title LIKE?", "%#{params[:title]}%")
   end
 end
