e59ca73a1e95d99c2711f30e2c7d1f60d001316f
DocPrior <Sara Prior>
2017-06-27 11:38:59 -0400

    change search to ILIKE

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
   def search_title
-    @projects = Project.where("title LIKE?", "%#{params[:title]}%")
+    @projects = Project.where("title ILIKE?", "%#{params[:title]}%")
   end
 end
